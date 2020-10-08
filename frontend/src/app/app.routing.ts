ee8325ea0e2dc8f1b1739bede1a517b76a3c0e28
SigneLuque17 <31870779+SigneLuque17@users.noreply.github.com>
2020-10-07 18:27:54 -0600

    actualizacion ruta inicio de sesión

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -5,12 +5,14 @@ import { Routes, RouterModule } from "@angular/router";
 //Importar componentes
 import { InicioComponent } from "./componentes/inicio/inicio.component";
 import {RegistroComponent} from "./componentes/registro/registro.component";
+import { IniciarSesionComponent } from './componentes/iniciar-sesion/iniciar-sesion.component';
+
 //Array de rutas
 const appRoutes: Routes = [
     {path: '', component: InicioComponent},
     {path: 'inicio', component: InicioComponent},
+    {path: 'iniciar-sesion', component: IniciarSesionComponent},
     {path: 'registro', component: RegistroComponent},
-
     {path: '**', component: InicioComponent}
 ];
 
