07bfb353c8b8582e460e18ce6e9b1b5b356f2ffd
ChristhoperP <43559198+ChristhoperP@users.noreply.github.com>
2020-10-09 14:14:51 -0600

    archivo conexion backend

---
+++ backend/src/conexion.js
@@ -0,0 +1,12 @@
+'use strict'
+const { Pool } = require('pg');
+
+module.exports = {
+    pool : new Pool({
+        host: 'localhost',
+        user: 'postgres',
+        password: 'postgres',
+        database: 'cactus',
+        port: 5432
+    })
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,15 @@
 
 var app = require('./app');
 var rutas = require('./config');
+const { pool } = require('./conexion');
 
 //starting the server
+pool.query('SELECT NOW()', (err, res) => { //probando la conexion a la bbdd
+    if(err) {console.log(err)}else{
+        console.log("Base de datos corriendo correctamente: " + res.rows[0].now)
+    }
+});
+
 app.listen(rutas.port, ()=>{
     console.log(`Server on port ${rutas.port}`);
 });
