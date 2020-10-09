4b79f6270668ec3db4e451be5d48c4014ab3dedd
antioch28 <45645363+antioch28@users.noreply.github.com>
2020-10-08 19:38:19 -0600

    Servicio de inicio de sesión
    Se creó el servicio para la petición de inicio de sesión y un archivo para mantener la url del servidor

--- frontend/src/app/servicios/auth.service.ts
+++ frontend/src/app/servicios/auth.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { SERVER_URL } from '../SERVER_URL'; // URL del servidor
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  URL = SERVER_URL;
+  constructor(private http: HttpClient) { }
+
+  iniciarSesion( usuario: Object){
+    return this.http.post<any>(this.URL + '/login', usuario);
+  }
+  
 }
