7b60ea840b3d914634abd2509ab2643881839daf
SigneLuque17 <31870779+SigneLuque17@users.noreply.github.com>
2020-10-09 13:18:26 -0600

    validación de formulario 1.2

--- frontend/src/app/componentes/registro/registro.component.ts
+++ frontend/src/app/componentes/registro/registro.component.ts
@@ -7,11 +7,25 @@ import {FormControl, FormGroup, Validators, MaxLengthValidator} from '@angular/f
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
+
+//comparar contraseñas
+contrasenaVerificada:boolean=false;
+habilitarBoton:boolean=false;
+
   formularioRegistro:FormGroup = new FormGroup({
     nombre: new FormControl('', [Validators.required]),
-    correo: new FormControl('', [Validators.required, Validators.pattern(/^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i)]),
-    contrasena: new FormControl('', [Validators.required, Validators.maxLength(20), Validators.minLength(8)]),
-    verificarContrasena: new FormControl('', [Validators.required, Validators.maxLength(20), Validators.minLength(8)])
+    correo: new FormControl('', [
+                                  Validators.required, 
+                                  Validators.pattern(/^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i)
+                                ]),
+    contrasena: new FormControl('',[
+                                  Validators.required,
+                                  Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&.])[A-Za-z\d$@$!%*?&].{8,20}')
+                                ]),
+    verificarContrasena: new FormControl('', [
+                                  Validators.required,
+                                  Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&.])[A-Za-z\d$@$!%*?&].{8,20}')
+                                ])
   });
   constructor() { }
 
@@ -20,4 +34,28 @@ export class RegistroComponent implements OnInit {
   validation(campo){
     return this.formularioRegistro.get(campo).invalid && this.formularioRegistro.get(campo).touched;
   }
+
+  compararContrasena(){
+      if (this.formularioRegistro.get('verificarContrasena').value === this.formularioRegistro.get('contrasena').value) {
+        console.log("contraseña verificada");
+        this.contrasenaVerificada = true;
+      } else{
+        this.contrasenaVerificada = false;
+        console.log("las contraseñas no coindicen");
+      }
+  }
+
+  registrarse(){
+    if (this.formularioRegistro.get('nombre').valid &&
+        this.formularioRegistro.get('nombre').valid &&
+        this.formularioRegistro.get('nombre').valid &&
+        this.contrasenaVerificada
+    ) {
+      console.log("cuenta nueva");
+      
+    } 
+    else {
+    }
+  }
+
 }
