0c87bf20c992bf6cce27abe086f4bf453a38f441
yohesler@gmail.com <43556496+HeslerG9@users.noreply.github.com>
2020-10-09 21:58:02 -0600

    Creacion de Servicios completos
    Creacion de Servicios completos de AUTH para guardar el token en el localstorage Y Token Interceptor

--- frontend/src/app/servicios/token-interceptor.service.ts
+++ frontend/src/app/servicios/token-interceptor.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpInterceptor } from "@angular/common/http";
+import { AuthService } from "../servicios/auth.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenInterceptorService {
 
-  constructor() { }
+  constructor(
+    private _AuthService: AuthService
+  ) { }
+
+  intercept(req, next){
+    const tokenizeReq = req.clone({
+      setHeaders: {
+        authorization: `Bearer ${this._AuthService.getToken()}`
+      }
+    });
+
+    return next.handle(tokenizeReq);
+  }
+
 }
