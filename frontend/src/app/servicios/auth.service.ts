ddc7c86062f9986c1a36d6363e2faaa05673b98e
antioch28 <45645363+antioch28@users.noreply.github.com>
2020-10-11 12:49:54 -0600

    Correciones en inicio de sesión
    Se corrigieron las validaciones de los campos y se modificó el servicio de autenticación

--- frontend/src/app/servicios/auth.service.ts
+++ frontend/src/app/servicios/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Router } from "@angular/router";
 import { Global } from "./global";
 
@@ -9,19 +9,21 @@ import { Global } from "./global";
 export class AuthService {
 
     private url = Global.url;
+    private headers = new HttpHeaders().set('Content-Type','application/json');
+    
     constructor(private _http: HttpClient, private _router: Router) { }
 
 
     signUp(user) {
         let params = JSON.stringify(user);
 
-        return this._http.post<any>(this.url + "registro", params);
+        return this._http.post<any>(this.url + "registro", params, {headers:this.headers});
     }
 
     iniciarSesion(user) {
         let params = JSON.stringify(user);
-
-        return this._http.post<any>(this.url + "validarUsuario", params);
+                        
+        return this._http.post<any>(this.url + "validarUsuario", params, {headers:this.headers});        
     }
 
     loggedIn() {
