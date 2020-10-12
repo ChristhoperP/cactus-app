b7aea6ee4c829436480036575b1b889a7d602749
ChristhoperP <43559198+ChristhoperP@users.noreply.github.com>
2020-10-11 21:16:18 -0600

    Backend: ruta validarToken pasó a ser perfil. middleware noAuth que verifica que el usuario no tenga una sesion iniciada, este se usa en la ruta del registro y validarUsuario. Se eliminó la ruta home y la función del mismo nombre del user controller

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -7,9 +7,7 @@ const auth = require('../middlewares/auth');
 
 var UserController = require('../controllers/user');
 
-
-router.get('/home', UserController.home);
-router.get('/verificatoken', auth.isAuth, (req, res) => { res.status(200).send({ verification: true, user: req.user, message: 'Tienes acceso' }) }); //prueba resticción de acceso a las rutas
-router.post('/registro', UserController.registrar);
-router.post('/validarUsuario', UserController.validarUsuario);
+router.get('/perfil', auth.isAuth, (req, res) => { res.status(200).send({ verification: true, user: req.user, message: 'Tienes acceso' }) }); //prueba resticción de acceso a las rutas
+router.post('/registro', auth.noAuth, UserController.registrar);
+router.post('/validarUsuario', auth.noAuth, UserController.validarUsuario);
 module.exports = router;
