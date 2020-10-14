a32eb7bfc3a4ba3058d8fae2a19190013492ccb9
ChristhoperP <43559198+ChristhoperP@users.noreply.github.com>
2020-10-14 16:46:37 -0600

    Se quitó el evento que borra el token al recargar la pagina

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +7,4 @@ import { Component, HostListener } from '@angular/core';
 })
 export class AppComponent {
   title = 'cactus';
-  @HostListener('window:beforeunload', ['$event'])
-    beforeunloadHandler(event) {
-    localStorage.clear();
-}
 }
