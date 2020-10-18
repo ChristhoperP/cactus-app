54c8aaaa9367cf4cfbcabaa5c54273f331e29db8
StephanyIS <stephanypineda16@gmail.com>
2020-10-17 23:18:56 -0600

    Categorias y tipos de bases (SP)
    Servicios para traer la información de categorias y tipos de bases

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -9,16 +9,24 @@ const image = require('../middlewares/images');
 var UserController = require('../controllers/user');
 var ImageController = require('../controllers/image');
 var AdminController = require('../controllers/admin');
-var VisitaController = require('../controllers/visita')
+var VisitaController = require('../controllers/visita');
+var productoController = require('../controllers/producto');
 
 router.get('/perfil', auth.isAuth, (req, res) => { res.status(200).send({ verification: true, user: req.user, message: 'Tienes acceso' }) }); //prueba resticción de acceso a las rutas
 router.post('/registro', auth.noAuth, UserController.registrar);
 router.post('/validarUsuario', auth.noAuth, UserController.validarUsuario);
+<<<<<<< HEAD
 
 
 //Subida de imagenes
 router.post('/upload-image/:id', image.upload.single('image'), ImageController.subirImagen);
 router.post('/upload-images/:id', image.upload.array('images', 2), ImageController.subirImagenes);
+=======
+router.post('/upload-image/:id', image.upload.single('image'), ImageController.SubirImagen);
+router.post('/upload-images/:id', image.upload.array('images', 2), ImageController.SubirImagenes);
+router.get('/tipos-bases', productoController.getTiposBases);
+router.get('/categorias', productoController.getCategorias);
+>>>>>>> Categorias y tipos de bases (SP)
 
 
 //Visitas por mes y usuarios
