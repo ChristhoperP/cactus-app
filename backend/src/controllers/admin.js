0713ef088697b9fb4d080b940ccd6129a95f208d
yohesler@gmail.com <43556496+HeslerG9@users.noreply.github.com>
2020-10-21 12:23:16 -0600

    Servicio registrar producto
    Terminado solo para revision

--- backend/src/controllers/admin.js
+++ backend/src/controllers/admin.js
@@ -157,7 +157,7 @@ var controller = {
             try {
 
                 const response = await pool.query(
-                    'SELECT SP_AGREGAR_PRODUCTO($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12);', [nombre, parseInt(categoria), parseInt(tipobase), parseInt(especie), parseInt(cantidad), parseFloat(precio), tiemposol, frecuenciariego, tamanio, informacionadicional, portada[0].filename, galeria[2].filename]
+                    'SELECT SP_AGREGAR_PRODUCTO($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14);', [nombre, parseInt(categoria), parseInt(tipobase), parseInt(especie), parseInt(cantidad), parseFloat(precio), tiemposol, frecuenciariego, tamanio, informacionadicional, portada[0].filename, galeria[0].filename,galeria[1].filename,galeria[2].filename]
                 );
 
                 var respuesta = response.rows[0].sp_agregar_producto;
