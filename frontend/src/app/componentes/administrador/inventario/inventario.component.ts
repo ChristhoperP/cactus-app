d36905dd951a8fa6a0d8711fa9af6999d3b9b054
antioch28 <45645363+antioch28@users.noreply.github.com>
2020-10-22 15:31:59 -0600

    Alert para eliminar productos
    Se agregó el SweetAlert para confirmar la eliminación de uno o varios productos

--- frontend/src/app/componentes/administrador/inventario/inventario.component.ts
+++ frontend/src/app/componentes/administrador/inventario/inventario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-inventario',
@@ -10,6 +11,48 @@ export class InventarioComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  deleteProduct(): void {
+    Swal.fire({
+      title: 'Desea eliminar este producto?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: `Eliminar`,
+      cancelButtonText: `Cancelar`,
+      confirmButtonColor: `#50a1a5`,
+      cancelButtonColor: `red`
+    }).then((result) => {
+
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: 'Producto eliminado!',
+          icon: 'success',
+          confirmButtonColor: `#50a1a5`
+        });
+      }
+    });
+  }
+
+  deleteProducts(): void {
+    Swal.fire({
+      title: 'Desea eliminar los productos seleccionados?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: `Eliminar`,
+      cancelButtonText: `Cancelar`,
+      confirmButtonColor: `#50a1a5`,
+      cancelButtonColor: `red`
+    }).then((result) => {
+
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: 'Productos eliminados!',
+          icon: 'success',
+          confirmButtonColor: `#50a1a5`
+        });
+      }
+    });
   }
 
 }
