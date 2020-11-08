a3321e57f5f33343ed3bb4212649ce8f04ac1f67
antioch28 <45645363+antioch28@users.noreply.github.com>
2020-11-08 17:00:29 -0600

    Correción de ruta de eventos
    Correción de la variable de url para escuchar eventos del servidor

--- frontend/src/app/servicios/global.ts
+++ frontend/src/app/servicios/global.ts
@@ -1,7 +1,7 @@
 export var Global = {
     // Produccion
     // url: "https://primeval-legacy-289419.uc.r.appspot.com/api/",
-    // eventsUrl: "primeval-legacy-289419.uc.r.appspot.com/events"
+    // eventsUrl: "https://primeval-legacy-289419.uc.r.appspot.com/events"
     //Desarrollo
     url: "http://localhost:3000/api/",
     eventsUrl: "http://localhost:3000/events"
