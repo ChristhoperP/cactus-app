bf170cdbf1f8bbb1f5384ce40bd7e0ee9e3df774
Alejandra <inessanchez_1996@hotmail,es>
2020-11-11 11:55:04 -0600

    Correcciones

--- frontend/src/app/servicios/administrador/serv-admin.service.ts
+++ frontend/src/app/servicios/administrador/serv-admin.service.ts
@@ -1,23 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { SERVER_URL } from '../../SERVER_URL';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Global } from '../global';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ServAdminService {
+  private url = Global.url;
+  private headers = new HttpHeaders().set('Content-Type', 'application/json');
   constructor(private http: HttpClient) {}
 
   getVisits() {
-    return this.http.get(`${SERVER_URL}/visita-usuario`);
+    return this.http.get(this.url + 'visita-usuario', {headers: this.headers});
   }
 
   getCantidadProducto() {
-    return this.http.get(`${SERVER_URL}/productos`);
+    return this.http.get(this.url + 'productos', {headers: this.headers});
   }
 
   getCategoria() {
-    return this.http.get(`${SERVER_URL}/productosCategoria`);
+    return this.http.get(this.url + 'productosCategoria', {headers: this.headers});
   }
 
   Carga (archivos:string[]){
