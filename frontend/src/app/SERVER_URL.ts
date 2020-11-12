e92fe6e94761c16bc136fff7390296f23fd40eaf
ChristhoperP <43559198+ChristhoperP@users.noreply.github.com>
2020-11-11 23:49:59 -0600

    modo produccion

--- frontend/src/app/SERVER_URL.ts
+++ frontend/src/app/SERVER_URL.ts
@@ -1,2 +1,2 @@
-//export const SERVER_URL = "https://primeval-legacy-289419.uc.r.appspot.com/api/"
-export const SERVER_URL = "http://localhost:3000/api/"
+export const SERVER_URL = "https://primeval-legacy-289419.uc.r.appspot.com/api/"
+//export const SERVER_URL = "http://localhost:3000/api/"
