6f12f76cf3cbcf52982bd1ba837e06f23da7e25f
Alejandra <inessanchez_1996@hotmail,es>
2020-11-16 18:35:31 -0600

    Cantidad

--- frontend/src/app/componentes/detalle-producto/detalle-producto.component.ts
+++ frontend/src/app/componentes/detalle-producto/detalle-producto.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {DetalleProductoService} from 'src/app/servicios/detalle-producto.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Global} from '../../servicios/global';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-detalle-producto',
@@ -14,6 +15,12 @@ export class DetalleProductoComponent implements OnInit {
   id:string;
   cargando:boolean=true;
 
+  formularioCaracteristicas: FormGroup = new FormGroup({ 
+    cantidad: new FormControl(null, [Validators.required, Validators.min(1), Validators.max(100)]),
+  });
+
+  
+   
 
   constructor(private ServicioDetalle: DetalleProductoService
     , private _route: ActivatedRoute,
@@ -32,9 +39,13 @@ export class DetalleProductoComponent implements OnInit {
       this.producto = data[0];
       this.cargando=false;
       console.log(data);
-    });
+    }
+    
+    );
+
+    
   }
 
-  
+  get cantidad(): AbstractControl { return this.formularioCaracteristicas.get('cantidad'); }
 
 }
