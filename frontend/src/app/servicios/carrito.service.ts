1425f7c38095d99d39bff02bc9485ca8fde6eec2
antioch28 <45645363+antioch28@users.noreply.github.com>
2020-11-18 17:43:18 -0600

    Agregrar a carrito no autenticado
    Se corrigió la estructura de la información que se guarda del carrito para usuarios no autenticados

--- frontend/src/app/servicios/carrito.service.ts
+++ frontend/src/app/servicios/carrito.service.ts
@@ -15,4 +15,33 @@ export class CarritoService {
     return this._http.get(this.url + 'productos-carrito/'+id);
 
   }
+
+  agregarCarritoNoLogged(producto): boolean {
+    let productosCarrito = { productos: [] };
+
+    if (!localStorage.getItem('productos-carrito')) {
+      productosCarrito.productos.push(producto);
+      localStorage.setItem('productos-carrito', JSON.stringify(productosCarrito.productos));
+      console.log('Agregó el producto con id ' + producto.idproducto + ' a su carrito de compras');
+      return true;
+    } else {
+
+      productosCarrito.productos = JSON.parse(localStorage.getItem('productos-carrito'));
+
+      if (productosCarrito.productos.findIndex( (prod: any) => prod.idproducto === producto.idproducto) > -1){
+        return false;
+      } else {
+        productosCarrito.productos.push(producto);
+        localStorage.setItem('productos-carrito', JSON.stringify(productosCarrito.productos));
+        console.log('Agregó el producto con id ' + producto.idproducto + ' a su carrito de compras');
+        return true;
+      }
+    }
+
+  }
+
+  agregarCarritoLogged(idproducto, cantidad) {
+    return this._http.post<any>(this.url + 'registro-carrito', {idproducto, cantidad} , {headers: this.headers});
+  }
+
 }
