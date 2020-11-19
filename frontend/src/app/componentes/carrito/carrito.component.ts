7589b5f5ac3dd2d2530045584675dd753845198f
SigneLuque17 <31870779+SigneLuque17@users.noreply.github.com>
2020-11-19 17:22:50 -0600

    eliminar producto del carrito
    localstorage

--- frontend/src/app/componentes/carrito/carrito.component.ts
+++ frontend/src/app/componentes/carrito/carrito.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { CarritoService } from '../../servicios/carrito.service';
 import { AuthService } from 'src/app/servicios/auth.service';
 import { Global } from "../../servicios/global";
+import {Location} from '@angular/common';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-carrito',
@@ -13,68 +16,103 @@ public url: string;
 
 userLogged: boolean;
 
-  productosCarrito: any = [];
-  productos: any = [
-    {"idproducto": 1,
-    "cantidadencarrito": 4,
-    "urlportada": "3ae45f54-10cf-478d-9e78-d04ad7cce1cc.jpg", 
-    "nombre": "Neon", 
-    "cantidadinventario": "10", 
-    "precio": 200, 
-    "porcentajedescuento":  " ",
-    "preciocondescuento":  200
-    },
-    {"idproducto": 2,
-    "cantidadencarrito": 1,
-    "urlportada": "31afb72a-8a43-4212-b8d2-7a94ad79b05f.jpg", 
-    "nombre": "Sempervivum", 
-    "cantidadinventario": "5", 
-    "precio": 250, 
-    "porcentajedescuento":  10,
-    "preciocondescuento":  225
-    }
-];
-  // carritoLocalStorage = JSON.parse(localStorage.getItem('productos-carrito'));
-  // productosLocalStorage = this.carritoLocalStorage.productos;
-
-  carritoLocalStorage = JSON.parse(localStorage.getItem('carrito'));
-  productosLocalStorage = this.carritoLocalStorage;
-
+  storageName = 'productos-carrito';
+  productosCarrito:any = [];
   totalPagar;
 
-  
-
-  constructor(private authService: AuthService, private _carritoService: CarritoService) { 
+  constructor(private authService: AuthService, private _carritoService: CarritoService, private _location: Location) { 
     this.url = Global.url;
     this.userLogged = this.authService.loggedIn();
   }
 
   ngOnInit(): void {
     if (this.userLogged) {
-      this.productosCarrito = this.productos;
-      this.calcularTotalPagar();
-
-    // this._carritoService.obtenerProductosCarrito()
-    // .subscribe(res => {
-    //   console.log(res);
-    //   this.productosCarrito = res;
-    //   this.calcularTotalPagar();
-    // });
+    this._carritoService.obtenerProductosCarrito()
+      .subscribe(res => {
+        console.log(res);
+        this.productosCarrito = res;
+        this.calcularTotalPagar();
+      });
     }else {
-      this.productosCarrito = this.productosLocalStorage;
-      this.calcularTotalPagar();
-       console.log(this.productosCarrito);
-
+      if (localStorage.getItem(this.storageName) === null) {
+        console.log("no hay productos en el carrito");
+      }else{
+        this.productosCarrito = JSON.parse(localStorage.getItem('productos-carrito'));
+        if (this.productosCarrito.length >=1) {
+          this.calcularTotalPagar();
+        }
+         console.log(this.productosCarrito);
+      }
     }
-
-
   }
 
   calcularTotalPagar(){
     var total=0;
     for (let i = 0; i < this.productosCarrito.length; i++) {
-      total += this.productosCarrito[i].preciocondescuento;
+      total += Number(this.productosCarrito[i].preciocondescuento);
     }
     this.totalPagar =total;    
+  }
+
+  eliminarTodosProductos(){
+    Swal.fire({
+      title: '¿Desea eliminar todos los productos?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: `Eliminar`,
+      cancelButtonText: `Cancelar`,
+      confirmButtonColor: `#50a1a5`,
+      cancelButtonColor: `red`
+    }).then((result) => {
+      if (result.isConfirmed) {
+        if (this.userLogged) {
+          console.log('logueado');
+          
+        }else{
+          localStorage.removeItem(this.storageName);
+          this.productosCarrito=[];
+          // this.totalPagar=0;
+        }
+      }
+    });
+  }
+
+  eliminarProductoCarrito( id: string ): void {
+    console.log('Id a eliminar: ' + id);
+    Swal.fire({
+        title: '¿Desea eliminar este producto?',
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonText: `Eliminar`,
+        cancelButtonText: `Cancelar`,
+        confirmButtonColor: `#50a1a5`,
+        cancelButtonColor: `red`
+      }).then((result) => {
+
+        if (result.isConfirmed) {
+          for (let i = 0; i < this.productosCarrito.length; i++) {
+            if ( this.productosCarrito[i].idproducto === id){
+              console.log('Eliminando: ' + id + ' / ' + this.productosCarrito[i]);
+              this.productosCarrito.splice(i, 1);
+                if (this.userLogged) {
+                  console.log('logueado');
+                  
+                }else{
+                  this.calcularTotalPagar();
+                  return this.actualizarLocalStorage();
+                }
+            }
+          }
+        }
+      });
+  }
+
+  private actualizarLocalStorage() {
+    localStorage.setItem(this.storageName, JSON.stringify(this.productosCarrito));
+    return this.productosCarrito;
+  }
+
+  regresar() {
+    this._location.back();
   }
 }
