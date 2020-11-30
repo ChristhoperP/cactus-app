b6ce8702e20787550451dd694377ea72c4b71af9
SigneLuque17 <31870779+SigneLuque17@users.noreply.github.com>
2020-11-30 00:30:07 -0600

    componentes reportes

---
+++ frontend/src/app/componentes/administrador/reportes/modales/filtro-inventario/filtro-inventario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filtro-inventario',
+  templateUrl: './filtro-inventario.component.html',
+  styleUrls: ['./filtro-inventario.component.css']
+})
+export class FiltroInventarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ frontend/src/app/componentes/administrador/reportes/modales/filtro-ventas/filtro-ventas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filtro-ventas',
+  templateUrl: './filtro-ventas.component.html',
+  styleUrls: ['./filtro-ventas.component.css']
+})
+export class FiltroVentasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
