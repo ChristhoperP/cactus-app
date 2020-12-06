c509de512cb4bec33eead041d2ec2a825e5a3105
SigneLuque17 <31870779+SigneLuque17@users.noreply.github.com>
2020-12-05 23:29:29 -0600

    filtros ventas
    funcionalidad

--- frontend/src/app/componentes/administrador/reportes/reporte-ventas/reporte-ventas.component.ts
+++ frontend/src/app/componentes/administrador/reportes/reporte-ventas/reporte-ventas.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import {Location} from '@angular/common';
+import { FilterPipe } from 'ngx-filter-pipe';
+
 
 @Component({
   selector: 'app-reporte-ventas',
@@ -9,21 +11,32 @@ import {Location} from '@angular/common';
 export class ReporteVentasComponent implements OnInit {
   ventas:any=[];
   cantidadTotal=0;
+  productosFilterByName = { nombre_producto: '' }; 
 
-  constructor(private _location: Location) {
+
+  constructor(private _location: Location, private filter: FilterPipe) {
     this.ventas;
+    this.productosFilterByName;
    }
 
   ngOnInit(): void {
   }
 
-  recibeVentas(ventas){
+  recibeVentas(ventas:any){
     console.log(ventas);
-    this.ventas=ventas;
-
+    this.ventas=ventas;    
     for (let i = 0; i < ventas.length; i++) {
       this.cantidadTotal+=ventas[i].cantidad_vendida;
      }
+  }
+
+  recibeNombreProducto(nombre){
+  this.productosFilterByName = { nombre_producto: nombre }; 
+  }
+
+  limpiarModal(){
+    this.ventas = [];
+    this.cantidadTotal=0;
   }
 
   regresar() {
